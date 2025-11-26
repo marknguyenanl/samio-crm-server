c5ebfef8f088967d6fd445320ce0f98f3c09078c
mark <marknguyenanl@gmail.com>
2025-11-26 22:37:06 +0700

    fix route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'auth',
+    'prefix' => 'crm/v1',
 ], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
