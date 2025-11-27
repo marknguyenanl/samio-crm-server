3ae845f33da3494c9e2e865c8ee31899c9f238c5
mark <marknguyenanl@gmail.com>
2025-11-27 07:59:32 +0700

    u

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['crm.anrostech.com', 'anrostech.com'],
-    'allowed_origins' => ['https://crm.anrostech.com', 'http://crm.anrostech.com', 'http://anrostech.com', 'https://anrostech.com'],
+    'allowed_origins' => ['https://crm.anrostech.com', 'https://anrostech.com'],
 
     'allowed_origins_patterns' => [],
 
