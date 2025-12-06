9fb3167bd4059bc7bfb81aea42455ac9092e1e72
mark <marknguyenanl@gmail.com>
2025-12-07 02:06:09 +0700

    p

--- config/cors.php
+++ config/cors.php
@@ -19,15 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [
-        // Allow localhost on any port
-        '/^http:\/\/localhost(:[0-9]+)?$/',
-
-        // Allow any subdomain of samiohome.com over http or https
-        '/^https?:\/\/([a-z0-9-]+\.)*samiohome\.com$/i',
-    ],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
