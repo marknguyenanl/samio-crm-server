82bc1c8ef540de2f5e59754d401963ceb1730dab
mark <marknguyenanl@gmail.com>
2025-12-28 09:40:27 +0700

    feat: add metrics controller

--- app/Http/Controllers/MetricController.php
+++ app/Http/Controllers/MetricController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -46,6 +47,78 @@ class MetricController extends Controller
         }
 
         $data = $query->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * Get the conversion rate for the current month.
+     *
+     * @return array<string,mixed>
+     */
+    public function getConversionRateThisMonth(): array
+    {
+        $thisMonth = Carbon::now()->month();
+        // find total leads in period
+        $subA = DB::table('contact_stage_histories')
+            ->select(
+                'contact_id',
+                DB::raw('DATE(MIN(created_at)) as adate')
+            )
+            ->where('contact_stage_id', 2)
+            ->whereMonth('created_at', $thisMonth)
+            ->groupBy('contact_id');
+
+        $subB = DB::table('contact_stage_histories')
+            ->select(
+                'contact_id',
+                DB::raw('DATE(MIN(created_at)) as bdate')
+            )
+            ->where('contact_stage_id', 4)
+            ->whereMonth('created_at', $thisMonth)
+            ->groupBy('contact_id');
+
+        $total_customers_this_month = DB::query()
+            ->fromSub($subA, 'a')
+            ->joinSub($subB, 'b', function ($join) {
+                $join->on('a.contact_id', '=', 'b.contact_id');
+            })
+            ->selectRaw('COUNT(*) as total_customers')
+            ->value('total_customers'); // returns integer
+        // get contact_id
+        // find corresponding contact_id customers stage
+        $total_leads_this_month = DB::query()
+            ->fromSub($subA, 'a')
+            ->selectRaw('COUNT(*) as total_leads')
+            ->value('total_leads'); // returns integer
+
+        // divide
+        $conversion_rate_this_month = $total_leads_this_month > 0
+            ? $total_customers_this_month / $total_leads_this_month
+            : 0;
+
+        return [
+            'conversion_rate' => $conversion_rate_this_month,
+            'total_leads' => $total_leads_this_month,
+            'total_customers' => $total_customers_this_month,
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse JSON response containing the token payload.
+     */
+    public function getConversionRate()
+    {
+        $type = 'this-month';
+        switch ($type) {
+            case 'this-month':
+            default:
+                $data = $this->getConversionRateThisMonth();
+                break;
+        }
 
         return response()->json([
             'success' => true,
